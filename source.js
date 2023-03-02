ebf2d4fe2e86e9f71bf9ecb3d5299d2750daeff0
Gabriel <52301750+GabrielOliveira666@users.noreply.github.com>
2023-03-02 20:23:55 -0300

    Add files via upload

--- source.js
+++ source.js
@@ -2,6 +2,13 @@ const DEFAULT_COLOR = 'rgb(10, 9, 9)';
 const DEFAULT_MODE = 'pen';
 const DEFAULT_SIZE = 16;
 
+const penBtn = document.getElementById('pen');
+const eraserBtn = document.getElementById('eraser');
+const clearBtn = document.getElementById('clear');
+const gridSizeBtn = document.getElementById('grid_range');
+const grid = document.getElementById('grid');
+const rainbowBtn = document.getElementById('rainbow');
+
 let currentMode = DEFAULT_MODE;
 let currentSize = DEFAULT_SIZE;
 let currentColor = DEFAULT_COLOR;
@@ -13,18 +20,12 @@ function setCurrentMode(newMode){
 
 function setCurrentSize(newSize){
     currentSize = newSize;
+    reloadGrid();
 }
 
 function setCurrentColor(newColor){
     currentColor = newColor;
 }
-
-const penBtn = document.getElementById('pen');
-const eraserBtn = document.getElementById('eraser');
-const clearBtn = document.getElementById('clear');
-const gridSizeBtn = document.getElementById('grid_range');
-const grid = document.getElementById('grid');
-const rainbowBtn = document.getElementById('rainbow');
 
 clearBtn.onclick = () => reloadGrid(); 
 penBtn.onclick = () => setCurrentMode('pen');
@@ -32,15 +33,19 @@ eraserBtn.onclick = () => setCurrentMode('eraser');
 rainbowBtn.onclick = () => setCurrentMode('rainbow');
 gridSizeBtn.onchange = (e) => changeSize();
 
+let mouseDown = false;
+
+//funções de alteração do grid
 function setGrid(size){
+    clear();
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
-
+    
     for(let i = 0; i < size * size; i++){
         const gridElement = document.createElement('div');
 
-        gridElement.addEventListener('mouseover', paint);
-        gridElement.addEventListener('mousedown', paint);
+        //gridElement.addEventListener('mouseover', paint);
+        //gridElement.addEventListener('mousedown', paint);
 
 
         gridElement.classList.add('grid-element');
@@ -53,28 +58,49 @@ function reloadGrid(){
     setGrid(currentSize);
 }
 
-function clear(){
-    grid.innerHTML = ``
-}
-
 function changeSize(){
     gridSizeBtn.onchange = (e) => setCurrentSize(e.target.value);
     setGrid(currentSize);
     reloadGrid();
 }
 
-function paint(e){
-    if (e.type === 'mouseover' && !mouseDown) return
-    if(currentMode == 'pen'){
-        e.target.style.backgroundColor = currentColor;
-    }
-    else if(currentMode == 'eraser'){
-        e.target.style.backgroundColor = 'rgb(255,255,255)';
-    }
+function clear(){
+    grid.innerHTML = ``
 }
+
+//funções do paint
 
 setGrid(currentSize)
 
+function paint(){
+    const gridElement = document.querySelectorAll('.grid-element');
+    console.log(gridElement[0])
+    
+    gridElement.forEach(elemento => {
+        elemento.addEventListener('mousedown', () => {
+            mouseDown = true;
+        })
+    })
+    
+    gridElement.forEach(elemento => {
+        elemento.addEventListener('mouseup', () => {
+            mouseDown = false;
+        })
+    })
+    
+    gridElement.forEach(elemento => {
+        elemento.addEventListener('mousemove', () => {
+            if(mouseDown == true && currentMode == 'pen'){
+                console.log('estou funcionando!')
+                elemento.style.backgroundColor = currentColor;
+            }else if(mouseDown == true && currentMode == 'eraser'){
+                elemento.style.backgroundColor = 'rgb(255,255,255)';
+            }
+        })
+    })    
+}
+
+//paint()
 
 
 
